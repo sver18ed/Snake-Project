f9775a15310ae8d3a12cafba883999bbc93506e8
Erik Sveningsson <sver18ed@student.ju.se>
2019-11-15 10:43:34 +0100

    addded xcode project

---
+++ SnakeProject/SnakeProject/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  SnakeProject
+//
+//  Created by Erik Sveningsson on 2019-11-15.
+//  Copyright © 2019 Erik Sveningsson. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+   
+}
---
+++ SnakeProject/SnakeProject/GameViewController.swift
@@ -0,0 +1,58 @@
+//
+//  GameViewController.swift
+//  SnakeProject
+//
+//  Created by Erik Sveningsson on 2019-11-15.
+//  Copyright © 2019 Erik Sveningsson. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            print();
+            
+            
+            
+            
+            
+            
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
