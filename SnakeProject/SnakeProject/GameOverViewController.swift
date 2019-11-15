cd83a825a02bac55d45c6958e34f7fae03dc0682
Erik Sveningsson <sver18ed@student.ju.se>
2019-11-15 11:41:39 +0100

    Fixed View Controllers

---
+++ SnakeProject/SnakeProject/GameOverViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameOverViewController.swift
+//  SnakeProject
+//
+//  Created by Erik Sveningsson on 2019-11-15.
+//  Copyright © 2019 Erik Sveningsson. All rights reserved.
+//
+
+import UIKit
+
+class GameOverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SnakeProject/SnakeProject/MainMenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainMenuViewController.swift
+//  SnakeProject
+//
+//  Created by Erik Sveningsson on 2019-11-15.
+//  Copyright © 2019 Erik Sveningsson. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
