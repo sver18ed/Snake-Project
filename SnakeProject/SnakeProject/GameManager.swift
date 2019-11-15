48d3460b371f9b0662d6e49520e003134e59de63
Andreas Malmström <maan1686@student.ju.se>
2019-11-15 13:00:19 +0100

    Tried out sample code to see if working

---
+++ SnakeProject/SnakeProject/GameManager.swift
@@ -0,0 +1,14 @@
+//
+//  GameManager.swift
+//  SnakeProject
+//
+//  Created by Andreas Malmström on 2019-11-15.
+//  Copyright © 2019 Erik Sveningsson. All rights reserved.
+//
+
+import SpriteKit
+
+class GameManager {
+    
+    
+}
--- SnakeProject/SnakeProject/GameScene.swift
+++ SnakeProject/SnakeProject/GameScene.swift
@@ -11,8 +11,50 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var game: GameManager!
     
+    var currentScore: SKLabelNode!
+    var PlayerPositions: [(Int, Int)] = []
+    var gameBG: SKShapeNode!
+    var gameArray: [(node: SKShapeNode, x: Int, y: Int)] = []
+    
+    override func didMove(to view: SKView) {
+        game = GameManager()
+        initializeGameView()
+    
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            let touchedNode = self.nodes(at: location)
+            
+                startGame ()
+            
+        }
+        
+    }
+    
+    private func startGame() {
+        print("Start Game")
+    }
+    
+    private func initializeGameView() {
+        currentScore = SKLabelNode(fontNamed: "ArialRoundedMTBold")
+        currentScore.zPosition = 1
+        currentScore.position = CGPoint(x: 0, y: (frame.size.height / -2) + 60)
+        currentScore.fontSize = 40
+        currentScore.text = "Score: 0"
+        currentScore.fontColor = SKColor.white
+        self.addChild(currentScore)
+        
+        let width = frame.size.width - 200
+        let height = frame.size.height - 300
+        
+    }
    
 }
