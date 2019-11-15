54421b4f5a6bae360791969b0fe7f40fd810ae0d
Andreas Malmström <maan1686@student.ju.se>
2019-11-15 15:03:54 +0100

    Made snake move with swipes

--- SnakeProject/SnakeProject/GameManager.swift
+++ SnakeProject/SnakeProject/GameManager.swift
@@ -10,5 +10,108 @@ import SpriteKit
 
 class GameManager {
     
+    var scene: GameScene!
+    
+    var nextTime: Double?
+    var timeExtension: Double = 0.15
+    var playerDirection: Int = 1
+    
+    init(scene: GameScene) {
+        self.scene = scene
+    }
+    func initGame() {
+        scene.PlayerPositions.append((10, 10))
+        scene.PlayerPositions.append((10,11))
+        scene.PlayerPositions.append((10, 12))
+        renderChange()
+    }
+    
+    func update(time: Double) {
+        if nextTime == nil {
+            nextTime = time + timeExtension
+        } else {
+            if time >= nextTime! {
+                nextTime = time + timeExtension
+                updatePlayerPosition()
+                
+            }
+        }
+    }
+    
+    private func updatePlayerPosition(){
+        var xChange = -1
+        var yChange = 0
+        
+        switch playerDirection {
+            case 1:
+                xChange = -1
+                yChange = 0
+                break
+            case 2:
+                xChange = 0
+                yChange = -1
+                break
+            case 3:
+                xChange = 1
+                yChange = 0
+                break
+            case 4:
+                xChange = 0
+                    yChange = 1
+                    break
+                default:
+                    break
+        }
+        if scene.PlayerPositions.count > 0 {
+            var start = scene.PlayerPositions.count - 1
+            while start > 0 {
+                scene.PlayerPositions[start] = scene.PlayerPositions[start - 1]
+                start -= 1
+            }
+            scene.PlayerPositions[0] = (scene.PlayerPositions[0].0 +
+                yChange, scene.PlayerPositions[0].1 + xChange)
+        }
+        if scene.PlayerPositions.count > 0 {
+            let x = scene.PlayerPositions[0].1
+            let y = scene.PlayerPositions[0].0
+            if y > 40 {
+                scene.PlayerPositions[0].0 = 0
+            } else if y < 0 {
+                scene.PlayerPositions[0].0 = 40
+            } else if x > 20 {
+                scene.PlayerPositions[0].1 = 0
+            } else if x < 0 {
+                scene.PlayerPositions[0].1 = 20
+            }
+        }
+        renderChange()
+    }
+    
+    
+    
+    func renderChange() {
+        for (node, x, y) in scene.gameArray {
+            if contains(a: scene.PlayerPositions, v: (x,y)) {
+                node.fillColor = SKColor.cyan
+            } else {
+                node.fillColor = SKColor.clear
+            }
+        }
+    }
+    
+    func contains(a:[(Int, Int)], v:(Int, Int)) -> Bool {
+        let (c1, c2) = v
+        for (v1, v2) in a { if v1 == c1 && v2 == c2 { return true}}
+        return false
+        
+    }
+    
+    func swipe(ID: Int) {
+        if !(ID == 2 && playerDirection == 4) && !(ID == 4 && playerDirection == 2) {
+            if !(ID == 1 && playerDirection == 3) && !(ID == 3 && playerDirection == 1) {
+                playerDirection = ID
+            }
+        }
+    }
     
 }
--- SnakeProject/SnakeProject/GameScene.swift
+++ SnakeProject/SnakeProject/GameScene.swift
@@ -19,28 +19,66 @@ class GameScene: SKScene {
     var gameArray: [(node: SKShapeNode, x: Int, y: Int)] = []
     
     override func didMove(to view: SKView) {
-        game = GameManager()
+        game = GameManager(scene: self)
         initializeGameView()
+        startGame()
+        
+        let swipeRight: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeR))
+        swipeRight.direction = .right
+        view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeL))
+        swipeLeft.direction = .left
+        view.addGestureRecognizer(swipeLeft)
+        
+        let swipeUp: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeU))
+        swipeUp.direction = .up
+        view.addGestureRecognizer(swipeUp)
+        
+        let swipeDown: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeD))
+        swipeDown.direction = .down
+        view.addGestureRecognizer(swipeDown)
     
+    }
+    
+    @objc func swipeR() {
+        game.swipe(ID: 3)
+    }
+    
+    @objc func swipeL() {
+        game.swipe(ID: 1)
+    }
+    
+    @objc func swipeU() {
+        game.swipe(ID: 2)
+    }
+    
+    @objc func swipeD() {
+        game.swipe(ID: 4)
     }
     
     override func update(_ currentTime: TimeInterval) {
+        game.update(time: currentTime)
+        
         
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let location = touch.location(in: self)
-            let touchedNode = self.nodes(at: location)
-            
-                startGame ()
-            
-        }
-        
-    }
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for touch in touches {
+//            let location = touch.location(in: self)
+//            let touchedNode = self.nodes(at: location)
+//            for node in touchedNode {
+//
+//                startGame()
+//
+//            }
+//        }
+//    }
     
     private func startGame() {
         print("Start Game")
+        self.game.initGame()
+        
     }
     
     private func initializeGameView() {
@@ -54,7 +92,43 @@ class GameScene: SKScene {
         
         let width = frame.size.width - 200
         let height = frame.size.height - 300
+        let rect = CGRect(x: -width / 2, y: -height / 2, width: width, height: height)
+        gameBG = SKShapeNode (rect: rect, cornerRadius: 0.02)
+        gameBG.fillColor = SKColor.darkGray
+        gameBG.zPosition = 2
+        gameBG.isHidden = false
+        self.addChild(gameBG)
+        
+        createGameBoard(width: Int(width), height: Int(height))
         
     }
+    
+    private func createGameBoard( width: Int, height: Int) {
+        let cellWidth: CGFloat = 27.5
+        let numRows = 40
+        let numCols = 20
+        var x = CGFloat(width / -2) + (cellWidth / 2)
+        var y = CGFloat(height / 2) - (cellWidth / 2)
+        
+        for i in 0...numRows - 1 {
+            for j in 0...numCols - 1 {
+                let cellNode = SKShapeNode(rectOf: CGSize(width: cellWidth, height: cellWidth))
+                cellNode.strokeColor = SKColor.black
+                cellNode.zPosition = 2
+                cellNode.position = CGPoint(x: x, y: y)
+                
+                gameArray.append((node: cellNode, x: i, y: j))
+                gameBG.addChild(cellNode)
+                x += cellWidth
+                
+            }
+            x = CGFloat(width / -2) + (cellWidth / 2)
+            y -= cellWidth
+        }
+        
+    }
+    
+    
+    
    
 }
