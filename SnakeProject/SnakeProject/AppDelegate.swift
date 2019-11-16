0c8e6348a70e69db856fbd2df40d9d11f064e31a
Andreas Persson <andreas@epaa.nu>
2019-11-16 16:47:28 +0100

    Minimal changes

--- SnakeProject/SnakeProject/AppDelegate.swift
+++ SnakeProject/SnakeProject/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let defaults = UserDefaults.standard
+        let defaultValue = ["highScore" : 0]
+        defaults.register(defaults: defaultValue)
+        
         return true
     }
 
--- SnakeProject/SnakeProject/GameScene.swift
+++ SnakeProject/SnakeProject/GameScene.swift
@@ -11,8 +11,169 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var gameLogo: SKLabelNode!
+    var highScore: SKLabelNode!
+    var playButton: SKShapeNode!
+    var game: GameManager!
+    var currentScore: SKLabelNode!
+    var playerPositions: [(Int, Int)] = []
+    var gameBG: SKShapeNode!
+    var gameArray: [(node: SKShapeNode, x: Int, y: Int)] = []
+    var scorePos: CGPoint?
     
+    
+    private func initializeMenu() {
+                //Create game title
+        gameLogo = SKLabelNode(fontNamed: "ArialRoundedMTBold")
+        gameLogo.zPosition = 1
+        gameLogo.position = CGPoint(x: 0, y: (frame.size.height / 2) - 500)
+        gameLogo.fontSize = 100
+        gameLogo.text = "Snake"
+        gameLogo.fontColor = SKColor.red
+        self.addChild(gameLogo)
+                //Create best score label
+        highScore = SKLabelNode(fontNamed: "ArialRoundedMTBold")
+        highScore.zPosition = 1
+        highScore.position = CGPoint(x: 0, y: gameLogo.position.y - 50)
+        highScore.fontSize = 30
+        highScore.text = "HighScore: \(UserDefaults.standard.integer(forKey: "highScore"))"
+        highScore.fontColor = SKColor.white
+        self.addChild(highScore)
+                //Create play button
+        playButton = SKShapeNode()
+        playButton.name = "play_button"
+        playButton.zPosition = 1
+        playButton.position = CGPoint(x: 0, y: (frame.size.height / -2) + 600)
+        playButton.fillColor = SKColor.green
+        let topCorner = CGPoint(x: -50, y: 50)
+        let bottomCorner = CGPoint(x: -50, y: -50)
+        let middle = CGPoint(x: 50, y: 0)
+        let path = CGMutablePath()
+        path.addLine(to: topCorner)
+        path.addLines(between: [topCorner, bottomCorner, middle])
+        playButton.path = path
+        self.addChild(playButton)
+    }
+
+
+    private func initializeGameView() {
+        
+        currentScore = SKLabelNode(fontNamed: "ArialRoundedMTBold")
+        currentScore.zPosition = 1
+        currentScore.position = CGPoint(x: 0, y: (frame.size.height / -2) + 60)
+        currentScore.fontSize = 40
+        currentScore.isHidden = true
+        currentScore.text = "Score: 0"
+        currentScore.fontColor = SKColor.white
+        self.addChild(currentScore)
+        let width = frame.size.width - 200
+        let height = frame.size.height - 300
+        let rect = CGRect(x: -width / 2, y: -height / 2, width: width, height: height)
+        gameBG = SKShapeNode(rect: rect, cornerRadius: 0.02)
+        gameBG.fillColor = SKColor.darkGray
+        gameBG.zPosition = 2
+        gameBG.isHidden = true
+        self.addChild(gameBG)
+        createGameBoard(width: Int(width), height: Int(height))
+    }
+    
+            //create a game board, initialize array of cells
+    private func createGameBoard(width: Int, height: Int) {
+        let cellWidth: CGFloat = 32.5
+        let numRows = 40
+        let numCols = 20
+        var x = CGFloat(width / -2) + (cellWidth / 2)
+        var y = CGFloat(height / 2) - (cellWidth / 2)
+                //loop through rows and columns, create cells
+        for i in 0...numRows - 1 {
+            for j in 0...numCols - 1 {
+                let cellNode = SKShapeNode(rectOf: CGSize(width: cellWidth, height: cellWidth))
+                cellNode.strokeColor = SKColor.black
+                cellNode.zPosition = 2
+                cellNode.position = CGPoint(x: x, y: y)
+                        //add to array of cells -- then add to game board
+                gameArray.append((node: cellNode, x: i, y: j))
+                gameBG.addChild(cellNode)
+                        //iterate x
+                x += cellWidth
+            }
+                    //reset x, iterate y
+            x = CGFloat(width / -2) + (cellWidth / 2)
+            y -= cellWidth
+        }
+    }
+    
+    override func didMove(to view: SKView) {
+        initializeMenu()
+        game = GameManager(scene: self)
+        initializeGameView()
+        
+        let swipeRight:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeR))
+        swipeRight.direction = .right
+        view.addGestureRecognizer(swipeRight)
+        let swipeLeft:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeL))
+        swipeLeft.direction = .left
+        view.addGestureRecognizer(swipeLeft)
+        let swipeUp:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeU))
+        swipeUp.direction = .up
+        view.addGestureRecognizer(swipeUp)
+        let swipeDown:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeD))
+        swipeDown.direction = .down
+        view.addGestureRecognizer(swipeDown)
+    }
+    
+    @objc func swipeR() {
+        game.swipe(ID: 3)
+    }
+    @objc func swipeL() {
+        game.swipe(ID: 1)
+    }
+    @objc func swipeU() {
+        game.swipe(ID: 2)
+    }
+    @objc func swipeD() {
+        game.swipe(ID: 4)
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+                //called before each frame is rendered
+        game.update(time: currentTime)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            let touchedNode = self.nodes(at: location)
+            for node in touchedNode {
+                if node.name == "play_button" {
+                    startGame()
+                }
+            }
+        }
+    }
+    
+    
+    private func startGame() {
+        print("start game")
+        gameLogo.run(SKAction.move(by: CGVector(dx: -50, dy: 600), duration: 0.5)) {
+            self.gameLogo.isHidden = true
+        }
+        playButton.run(SKAction.scale(to: 0, duration: 0.3)) {
+            self.playButton.isHidden = true
+        }
+        let bottomCorner = CGPoint(x: 0, y: (frame.size.height / -2) + 20)
+        highScore.run(SKAction.move(to: bottomCorner, duration: 0.4))
+        highScore.run(SKAction.move(to: bottomCorner, duration: 0.4)) {
+            self.gameBG.setScale(0)
+            self.currentScore.setScale(0)
+            self.gameBG.isHidden = false
+            self.currentScore.isHidden = false
+            self.gameBG.run(SKAction.scale(to: 1, duration: 0.4))
+            self.currentScore.run(SKAction.scale(to: 1, duration: 0.4))
+            self.game.initGame()
+        }
+    }
+
    
 }
