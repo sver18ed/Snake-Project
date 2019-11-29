2e35e0345828e2a3054f63d04429541dc45668c8
Andreas Malmström <maan1686@student.ju.se>
2019-11-29 12:18:15 +0100

    Restored stuff from stashed

--- SnakeProject/SnakeProject/DataHandler.swift
+++ SnakeProject/SnakeProject/DataHandler.swift
@@ -12,4 +12,14 @@ class DataHandler {
     
     static let instance = DataHandler()
     var currentScore: Int = 0
+    var bestScore: Int = 0
+    
+    func setBestScore() {
+        if currentScore > bestScore{
+            bestScore = currentScore
+        }
+    }
+    func resetCurrentScore() {
+        currentScore = 0
+    }
 }
